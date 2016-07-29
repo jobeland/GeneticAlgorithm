47ed09bb0ebd88e7a74b5714d56499328a316dec
jobeland <jordan.beland@gmail.com>
2016-07-28 23:48:17 -0700

    udpate training sessions to support non-idempotent sessions

--- NeuralNetwork.GeneticAlgorithm/TrainingSession.cs
+++ NeuralNetwork.GeneticAlgorithm/TrainingSession.cs
@@ -7,48 +7,45 @@ namespace NeuralNetwork.GeneticAlgorithm
 
     public class TrainingSession : ITrainingSession
     {
-        private int _sessionNumber;
+        private readonly int _sessionNumber;
         public INeuralNetwork NeuralNet { get; set; }
-        private IEvaluatable _evaluatable;
+        private readonly IEvaluatable _evaluatable;
         private bool _hasStoredSessionEval;
         private double _sessionEval;
+        private readonly bool _isIdempotent;
 
-        public TrainingSession(INeuralNetwork nn, IEvaluatable evaluatable, int sessionNumber)
+        public TrainingSession(INeuralNetwork nn, IEvaluatable evaluatable, int sessionNumber, bool isIdempotent = true)
         {
             NeuralNet = nn;
             _evaluatable = evaluatable;
             _sessionNumber = sessionNumber;
             _hasStoredSessionEval = false;
             _sessionEval = 0;
+            _isIdempotent = isIdempotent;
         }
 
         public void Run()
         {
-            LoggerFactory.GetLogger().Log(LogLevel.Debug, string.Format("Starting training session {0}", _sessionNumber));
-            //only run the session if it hasn't already been run before
-            if (!_hasStoredSessionEval)
+            LoggerFactory.GetLogger().Log(LogLevel.Debug, $"Starting training session {_sessionNumber}");
+            //only run the session if it hasn't already been run before in idempotent sessions
+            if (!_hasStoredSessionEval || !_isIdempotent)
             {
                 _evaluatable.RunEvaluation();
             }
-            LoggerFactory.GetLogger().Log(LogLevel.Debug, string.Format("Stopping training session {0}", _sessionNumber));
+
+            LoggerFactory.GetLogger().Log(LogLevel.Debug, $"Stopping training session {_sessionNumber}");
         }
 
         public double GetSessionEvaluation()
         {
 
-            if (_hasStoredSessionEval)
+            if (_hasStoredSessionEval && _isIdempotent)
             {
                 return _sessionEval;
             }
-            else
-            {
-                _sessionEval = _evaluatable.GetEvaluation();
-                _hasStoredSessionEval = true;
-            }
+            _sessionEval = _evaluatable.GetEvaluation();
+            _hasStoredSessionEval = true;
             return _sessionEval;
         }
-
-
-
     }
 }
