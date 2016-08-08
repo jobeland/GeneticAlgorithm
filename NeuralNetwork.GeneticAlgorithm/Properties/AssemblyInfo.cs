e8b53c136c872cced760740599a21bfa7a0e2d42
jobeland <jordan.beland@gmail.com>
2016-08-07 21:57:45 -0700

    update package description

--- NeuralNetwork.GeneticAlgorithm/Properties/AssemblyInfo.cs
+++ NeuralNetwork.GeneticAlgorithm/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NeuralNetwork.GeneticAlgorithm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Genetic algorithm for evolving neural networks")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NeuralNetwork.GeneticAlgorithm")]
